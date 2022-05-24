973a6a2c6436a68697c597631688c26742c8a9a1
spensieri-s <stefano.spensieri@docity.com>
2022-05-24 05:24:16 +0200

    Changed some classnames

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import "./styles.css";
 import Card from "./components/Card";
 import CardV2 from "./components/CardV2";
 
-import data from "../utils/data.json";
+import data from "./utils/data.json";
 
 export default function App() {
   return (
     <div className="App">
-      <h1 class="pc-mainTitle">Let's get to know each other (v5)</h1>
-      <h2 class="pc-mainSubtitle">We can build a card to present ourselves</h2>
+      <h1 className="pc-mainTitle">Let's get to know each other (v5)</h1>
+      <h2 className="pc-mainSubtitle">We can build a card to present ourselves</h2>
       <div className="pc-card-container">
         {data.map((elem, index) => {
-          return <CardV2 key={`${elem.id}_${index}`} elem={elem} />;
-        })}
-        <div>
-          <h3>Old version:</h3>
-        </div>
-        {data.map((elem, index) => {
-          return <Card key={`${elem.id}_${index}`} elem={elem} />;
+          return <CardV2 key={`${elem.id}_${index}`} elem={elem} />
         })}
       </div>
     </div>
